f9838dfa51b19f670657eb7d247bba40d83a574b
fuka-takashima <fuka.takashima@dena.com>
2024-09-02 16:27:09 +0900

    Initial Commit

---
+++ sizeClassSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sizeClassSampleApp
+//
+//  Created by Fuka Takashima on 2024/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
